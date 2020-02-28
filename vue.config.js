00511c3af37984f68031f1c334a6998070cebf73
eldomaster <eldomaster@googlemail.com>
2020-02-28 16:26:14 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/proof-of-vue' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/proof-of-vue/docs' : '/',
   outputDir: './docs',
 };
