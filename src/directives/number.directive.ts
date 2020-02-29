f7182d15eccc059f5a9b76bef8eb71c1f5982cae
eldomaster <eldomaster@googlemail.com>
2020-02-29 19:49:17 +0100

    new build

--- src/directives/number.directive.ts
+++ src/directives/number.directive.ts
@@ -4,7 +4,6 @@ export const numberDirective: DirectiveOptions = {
   bind(el: HTMLElement) {
     const elRef = el as HTMLInputElement;
     elRef.addEventListener('input', () => {
-      console.log(el);
       const regex = /^[0-9]*$/;
       if (!regex.test(elRef.value)) {
         elRef.value = elRef.value.slice(0, -1);
